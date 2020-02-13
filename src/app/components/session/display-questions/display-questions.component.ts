b21aeac62ff969f62740b13511eaac561d2af108
Kenan Šabanović <sabanovic.kenan@gmail.com>
2020-02-13 13:22:19 +0100

    pagination active color when switching pages

--- src/app/components/session/display-questions/display-questions.component.ts
+++ src/app/components/session/display-questions/display-questions.component.ts
@@ -10,10 +10,23 @@ export class DisplayQuestionsComponent implements OnInit {
   @Input() question: Question;
   @Output() selectedAnswers = new EventEmitter<Map<number, number>>();
   answers: Map<number, number> = new Map<number, number>();
+  currentIndexChange: EventEmitter<number> = new EventEmitter();
+  status = false;
+  private _currentIndex: number;
 
   constructor() {}
 
+  set currentIndex(val: number) {
+    this.currentIndexChange.emit(val);
+    this._currentIndex = val;
+  }
+
+  get currentIndex(): number {
+    return this._currentIndex;
+  }
+
   selectAnswer(questionId: number, answerId: number) {
+    this.status = true;
     this.answers.set(questionId, answerId);
     this.selectedAnswers.emit(this.answers);
   }
