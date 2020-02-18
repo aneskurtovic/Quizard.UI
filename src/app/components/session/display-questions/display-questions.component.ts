9cd6fa5f8a1be4aeae78d3b14aac03dd0b88b4d2
Ana-Marija <knezevic.anamarija1@gmail.com>
2020-02-18 13:05:15 +0100

    Multiple choice added

--- src/app/components/session/display-questions/display-questions.component.ts
+++ src/app/components/session/display-questions/display-questions.component.ts
@@ -8,14 +8,42 @@ import { Question } from './../../../models/question';
 })
 export class DisplayQuestionsComponent implements OnInit {
   @Input() question: Question;
-  @Output() selectedAnswers = new EventEmitter<Map<number, number>>();
-  answers: Map<number, number> = new Map<number, number>();
-
+  @Input() numberOfQuestions: number;
+  @Output() selectedAnswers = new EventEmitter<Map<number, number[]>>();
+  @Output() submitButton = new EventEmitter<boolean>();
+  answers: Map<number, number[]> = new Map<number, number[]>();
+  answerIDs: number[] = [];
   constructor() {}
 
+  submited() {
+    this.submitButton.emit();
+  }
   selectAnswer(questionId: number, answerId: number) {
-    this.answers.set(questionId, answerId);
+    if (this.answers.has(questionId)) {
+      if (this.answers.get(questionId).includes(answerId)) {
+        const index = this.answers.get(questionId).indexOf(answerId);
+        this.answers.get(questionId).splice(index, 1);
+        if (this.answers.get(questionId).length === 0) {
+          this.answers.delete(questionId);
+        }
+        return;
+      }
+      this.answerIDs = this.answers.get(questionId);
+      this.answerIDs.push(answerId);
+    } else {
+      this.answerIDs = [];
+      this.answerIDs.push(answerId);
+    }
+    this.answers.set(questionId, this.answerIDs);
     this.selectedAnswers.emit(this.answers);
   }
   ngOnInit() {}
+
+  hasAnswer(questionId: number): boolean {
+    return this.answers.has(questionId);
+  }
+
+  isSelectedAnswer(questionId: number, answerId: number) {
+    return this.hasAnswer(questionId) && this.answers.get(questionId).includes(answerId);
+  }
 }
--- src/app/components/session/display-result/display-result.component.ts
+++ src/app/components/session/display-result/display-result.component.ts
@@ -8,28 +8,39 @@ import { Question } from '../../../models/question';
 export class DisplayResultComponent implements OnInit {
   @Input() quizResults: any;
   @Input() questions: Question[];
-  @Input() selectedAnswers: Map<number, number>;
+  @Input() selectedAnswers: Map<number, number[]>;
 
   constructor() {}
 
   ngOnInit() {}
 
   isCorrectAnswered(id: number): boolean {
-    if (this.selectedAnswers.get(id) === this.quizResults.correctQuestions[id]) {
-      return true;
+    const answers = this.selectedAnswers.get(id);
+    const correctAnswers = this.quizResults.correctQuestions[id];
+
+    for (const sa of answers) {
+      let incorrect = true;
+      correctAnswers.forEach(ca => {
+        if (sa === ca) {
+          incorrect = false;
+        }
+      });
+      if (incorrect) {
+        return false;
+      }
     }
-    return false;
+    return true;
   }
 
   isSelected(aId: number, qId: number): boolean {
-    if (this.selectedAnswers.get(qId) === aId) {
+    if (this.selectedAnswers.get(qId).includes(aId)) {
       return true;
     }
     return false;
   }
 
   isCorrectAnswer(aId: number, qId: number): boolean {
-    if (this.quizResults.correctQuestions[qId] === aId) {
+    if (this.quizResults.correctQuestions[qId].includes(aId)) {
       return true;
     }
     return false;
